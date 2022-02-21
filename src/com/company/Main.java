a50ab73e0251f48e887cbde3a56d8ef88ed3366d
Kaan <kaanmol@outlook.com>
2022-02-21 04:02:03 +0100

    Updated demo with vw and vh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,11 @@
 package com.company;
 
-import java.util.ArrayList;
 import com.company.infinity.Button;
 import com.company.infinity.Infinity;
-import com.company.infinity.Node;
-import org.jfree.fx.FXGraphics2D;
+import com.company.infinity.Unit;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -21,23 +17,25 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("InfinityWindow");
         
         Infinity infinity = new Infinity(800, 600);
-        
-        TabPane pane = new TabPane();
-        Tab tab = new Tab("schedule");
-        tab.setClosable(false);
-        tab.setContent(new Group(Infinity.instance));
-        pane.getTabs().add(tab);
-        primaryStage.setScene(new Scene(pane));
+        Scene scene = new Scene(new Group(Infinity.instance));
+
+        primaryStage.setScene(scene);
+        primaryStage.widthProperty().addListener((observable, oldValue, newValue) -> {
+            infinity.setWidth(scene.getWidth());
+        });
+        primaryStage.heightProperty().addListener((observable, oldValue, newValue) -> {
+            infinity.setHeight(scene.getHeight());
+        });
 
         primaryStage.show();
         infinity.start();
 
-        Button create = new Button(0, 0, 100, 50, "Create");
-        Button edit = new Button(101, 0, 100, 50, "Edit");
-        Button read = new Button(202, 0, 150, 50, "Read");
+        Button create = new Button(0, 0, Unit.px(100), Unit.vh(50), "Create");
+        Button edit = new Button(101, 0, Unit.px(100), Unit.px(50), "Edit");
+        Button read = new Button(202, 0, Unit.vw(50), Unit.vh(50), "Read");
 
         create.onMouseClick(e -> {
             System.out.println("Button clicked!");
