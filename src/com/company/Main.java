142bc1a975e7e108ba85a988075700e5c3746273
Kaan <kaanmol@outlook.com>
2022-02-23 22:27:41 +0100

    Added dynamic rendering of arena's and basic table cell

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,13 @@
 package com.company;
 
 import java.io.File;
+import java.time.LocalTime;
 
 import com.company.infinity.Button;
 import com.company.infinity.Infinity;
 import com.company.infinity.Sound;
 import com.company.infinity.Table;
+import com.company.infinity.TableCell;
 import com.company.infinity.Unit;
 import com.company.popup.ArenaPopup;
 import com.company.popup.BattlePopup;
@@ -13,6 +15,7 @@ import com.company.popup.TrainerPopup;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.TableRow;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -49,8 +52,13 @@ public class Main extends Application {
         Button musicPlay = new Button(101, 0, Unit.px(100), Unit.px(50), "Play Music");
         Button musicPause = new Button(202, 0, Unit.px(100), Unit.px(50), "Pause Music");
         new Arena("School");
+        new Arena("BattleArena 1");
+        new Arena("BattleArena 2");
+
         new Trainer("Niels", "Dirk");
         new Trainer("Owen", "Kaan");
+
+        new Battle(LocalTime.parse("10:00"), LocalTime.parse("12:00"), Arena.list.get(0), Trainer.list.get(0), Trainer.list.get(1));
 
         // Button create = new Button(0, 0, Unit.px(100), Unit.vh(50), "Create");
         // Button edit = new Button(101, 0, Unit.px(100), Unit.px(50), "Edit");
@@ -74,46 +82,26 @@ public class Main extends Application {
             sound.player.pause();
         });
 
-        Table table = new Table(0, 51, Unit.vw(100), Unit.vh(100).subtract(Unit.px(50)));
-        table.addColumn("");
-        table.addColumn("Arena 1");
-        table.addColumn("Arena 2");
-        table.addColumn("Arena 3");
-        table.addColumn("Arena 4");
-        table.addColumn("Arena 5");
-
         int minHour = 9;
         int maxHour = 20;
+
+        Table table = new Table(0, 51, Unit.vw(100), Unit.vh(100).subtract(Unit.px(50)));
+        
+        table.addColumn("");
+        for (int arena = 0; arena < Arena.list.size(); arena++) {
+            table.addColumn("Arena " + Arena.list.get(arena).getArenaName());
+        }
 
         table.addRow("");
         for (int i = minHour; i < maxHour; i++) {
             table.addRow(i + "");
         }
 
-        // table.addRow("9");
-        // table.addRow("10");
-        // table.addRow("11");
-        // table.addRow("12");
-        // table.addRow("12");
-        // table.addRow("12");
+        Battle battle = Battle.list.get(0);
+        int xMultiplier = battle.getEndTime().getHour() - battle.getBeginTime().getHour();
+        TableCell cell = new TableCell(battle.getTrainer1().getName() + " vs " + battle.getTrainer2().getName());
+        
+        table.addCell(0, battle.getBeginTime().getHour() - minHour, xMultiplier, 0, cell);
 
-        System.out.println();
-
-        // table.addRow("Lol");
-//        Table table = new Table(0, 0, Unit.vw(100), Unit.vh(100));
-//        table.addHeader("Lol");
-//        table.addHeader("Lol");
-//        table.addHeader("Lol");
-//        table.addHeader("Lol");
-//        table.addHeader("Lol");
-//        table.addHeader("Lol");
-//
-//        table.addRow("Lol");
-//        table.addRow("Lol");
-//        table.addRow("Lol");
-//        table.addRow("Lol");
-//        table.addRow("Lol");
-//        table.addRow("Lol");
-//        table.addRow("Lol");
     }
 }
