acc0d91c5d21c7a91e090c923f24956bb03540e3
Kaan <kaanmol@outlook.com>
2022-02-23 21:03:26 +0100

    Added subtract method to Unit

--- src/com/company/infinity/Unit.java
+++ src/com/company/infinity/Unit.java
@@ -5,12 +5,19 @@ public class Unit {
     private static final int VIEW_HEIGHT = 1;
     private static final int PIXEL = 2;
 
-    private int value;
-    private int unit;
+    private int value = 0;
+    private int unit = 0;
+    private int addition = 0;
 
     private Unit(int value, int unit) {
         this.value = value;
         this.unit = unit;
+    }
+
+    private Unit(int value, int unit, int addition) {
+        this.value = value;
+        this.unit = unit;
+        this.addition = addition;
     }
 
     public static Unit vw(int viewWidth) {
@@ -24,15 +31,20 @@ public class Unit {
     public static Unit px(int pixel) {
         return new Unit(pixel, Unit.PIXEL);
     }
+
+    public Unit subtract(Unit unit) {
+        this.addition = -1 * unit.getValue();
+        return this;
+    }
     
     public int getValue() {
         switch (this.unit) {
             case Unit.VIEW_HEIGHT:
-                return (int)(Infinity.instance.getHeight() / 100.0 * (double)this.value);
+                return (int)(Infinity.instance.getHeight() / 100.0 * (double)this.value) + addition;
             case Unit.VIEW_WIDTH:
-                return (int)(Infinity.instance.getWidth() / 100.0 * (double)this.value);
+                return (int)(Infinity.instance.getWidth() / 100.0 * (double)this.value) + addition;
             case Unit.PIXEL:
-                return this.value;
+                return this.value + addition;
             default:
                 return 0;
         }
