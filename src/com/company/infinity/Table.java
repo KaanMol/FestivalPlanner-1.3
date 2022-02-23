c75a45c824911b4dab889bd4a78aa813f4d31c91
Kaan <kaanmol@outlook.com>
2022-02-23 22:26:53 +0100

    Added base for table cells

--- src/com/company/infinity/Table.java
+++ src/com/company/infinity/Table.java
@@ -5,8 +5,8 @@ import org.jfree.fx.FXGraphics2D;
 import java.awt.Color;
 
 public class Table extends Node {
-    private Unit rowWidth = Unit.vw(10);
-    private Unit columnHeight = Unit.vh(10);
+    private Unit rowHeight = Unit.px(0);
+    private Unit columnWidth = Unit.px(0);
     private ArrayList<String> columns = new ArrayList<>();
     private ArrayList<String> rows = new ArrayList<>();
 
@@ -28,67 +28,47 @@ public class Table extends Node {
         this.rows.add(row);
     }
 
-    public void addChild(int xIndex, int yIndex, Node child) {
-        child.x = xIndex;
-        child.y = yIndex;
+    public void addCell(int xIndex, int yIndex, int xMultiplier, int yMultiplier, TableCell child) {
+        child.x = xIndex + this.columnWidth.getValue();
+        child.y = yIndex + this.rowHeight.getValue();
+        child.width = Unit.px(this.columnWidth.getValue() * xMultiplier);
+        child.height = Unit.px(this.rowHeight.getValue() * yMultiplier);
+        child.zIndex = this.zIndex + 1;
+        System.out.println(Infinity.instance.nodeList.nodes.size());
         Infinity.instance.nodeList.add(child);
+        System.out.println(Infinity.instance.nodeList.nodes.size());
+    }
+
+    @Override
+    public void update() {
+        this.columnWidth = Unit.px(this.width.getValue() / this.columns.size());
+        this.rowHeight = Unit.px(this.height.getValue() / this.rows.size());
     }
 
     @Override
     public void draw() {
         FXGraphics2D context = Infinity.instance.context;
 
-        int columnWidth = this.width.getValue() / this.columns.size();
-        int rowHeight = this.height.getValue() / this.rows.size();
-
-        Color[] color = { Color.RED, Color.GREEN, Color.BLUE, Color.ORANGE, Color.CYAN, Color.MAGENTA };
-
         for (int rows = 0; rows < this.rows.size(); rows++) {
             for (int columns = 0; columns < this.columns.size(); columns++) {
-                int leftX = this.x + columnWidth * columns;
-                int topY = this.y + rowHeight * rows;
-                int width = columnWidth;
-                int height = rowHeight;
-
-                
+                int leftX = this.x + columnWidth.getValue() * columns;
+                int topY = this.y + rowHeight.getValue() * rows;
+                int width = columnWidth.getValue();
+                int height = rowHeight.getValue();
 
                 if (rows == 0 || columns == 0) {
                     context.setColor(Color.LIGHT_GRAY);
                     context.fillRect(leftX, topY, width, height);
-                //     // leftX = this.x + columnWidth * columns;
-                //     // topY = this.y + rowHeight * rows;
-
-                //     // if (rows == 0 && columns == 0) {
-                //     //     leftX = this.x;
-                //     //     topY = this.y;
-                //     //     width = 100;
-                //     //     height = 50;
-                //     // }
-
-                //     // if (rows == 0) {
-                //     //     width = columnWidth;
-                //     //     height = 50;
-                //     // }
-
-                //     // if (columns == 0) {
-                //     //     height = rowHeight;
-                //     //     width = 100;
-                //     // }
                 } else {
                     context.setColor(Color.LIGHT_GRAY);
                     context.drawRect(leftX, topY, width, height);
-                    // context.setColor(color[(rows + columns) % color.length]);
                 }
-                // context.fillRect(leftX, topY, width, height);
+
                 context.setColor(Color.BLACK);
                     
                 if (rows == 0 || columns == 0) {
-                    //draw columns on top and hours on left and center the text
                     context.drawString(this.columns.get(columns), leftX + (width / 2) - (context.getFontMetrics().stringWidth(this.columns.get(columns)) / 2), topY + (height / 2) + (context.getFontMetrics().getHeight() / 2));
                     context.drawString(this.rows.get(rows), leftX + (width / 2) - (context.getFontMetrics().stringWidth(this.columns.get(columns)) / 2), topY + (height / 2) + (context.getFontMetrics().getHeight() / 2));
-
-                    // context.drawString(this.columns.get(columns), leftX + (width / 2) - (context.getFontMetrics().stringWidth(this.columns.get(columns)) / 2), topY + (height / 2) + (context.getFontMetrics().getHeight() / 2));
-                    // context.drawString(this.rows.get(rows), leftX + width / 2, topY + height / 2);
                 }
             }
         }
