9f686f01ff93bbaa441e1574104ad3a2e3b6d04b
Niels Leest <n.leest1@student.avans.nl>
2022-03-23 18:40:52 +0100

    Added json library

--- src/com/company/simulation/TileMap.java
+++ src/com/company/simulation/TileMap.java
@@ -1,13 +1,32 @@
 package com.company.simulation;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Scanner;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
 
 public class TileMap {
     private ArrayList<MapLayer> layers = new ArrayList<>();
     private ArrayList<MapArea> areas = new ArrayList<>();
 
-    public TileMap(File jsonFile) {
-        // TODO add JSON import
+    public TileMap(File file) {
+        StringBuilder json = new StringBuilder();
+        try {
+            Scanner s = new Scanner(file);
+            while (s.hasNextLine()) {
+                json.append(s.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        JsonReader reader = Json.createReader(new StringReader(json.toString()));
+        // TODO read json
     }
 }
