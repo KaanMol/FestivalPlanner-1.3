569c388b95540e69542fae9e5d4ac1d8fc3acb92
Niels Leest <n.leest1@student.avans.nl>
2022-03-23 19:08:56 +0100

    Added conversion of layers in json to mapLayers

--- src/com/company/simulation/TileMap.java
+++ src/com/company/simulation/TileMap.java
@@ -1,5 +1,6 @@
 package com.company.simulation;
 
+import java.awt.geom.Point2D;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.StringReader;
@@ -27,6 +28,29 @@ public class TileMap {
         }
 
         JsonReader reader = Json.createReader(new StringReader(json.toString()));
-        // TODO read json
+
+        int width = reader.readObject().getInt("width");
+        int height = reader.readObject().getInt("height");
+        JsonArray jsonLayers = reader.readObject().getJsonArray("layers");
+        for (JsonValue jsonLayer : jsonLayers) {
+            addLayer(jsonLayer.asJsonObject(), width, height);
+        }
+    }
+
+    private void addLayer(JsonObject layer, int width, int height) {
+        if (layer.getString("type").equals("tilelayer")) {
+            MapLayer mapLayer = new MapLayer(layers.size());
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    int type = layer.getJsonArray("data").getInt(x + y * width) - 1;
+                    if (type != -1){
+                        mapLayer.setTile(new MapTile(new Point2D.Double(x * 16, y * 16), null), x, y);
+                    }
+                }
+            }
+            layers.add(mapLayer);
+        } else {
+            //TODO handle object layer
+        }
     }
 }
