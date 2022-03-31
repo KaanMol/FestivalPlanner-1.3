b99eefd3e1e5e8dc9017590787458130e6234f00
Wouter <wouter.den.hollander@hotmail.com>
2022-03-31 16:37:40 +0200

    Created NPC class

---
+++ src/com/company/simulation/NPC.java
@@ -0,0 +1,13 @@
+package com.company.simulation;
+
+import com.company.infinity.Node;
+
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class NPC extends Node {
+    private Point2D position;
+    private ArrayList<BufferedImage> sprites;
+
+}
