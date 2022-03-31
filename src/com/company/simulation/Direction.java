19873195f9d4b9b58808704ca94087ee35311853
Wouter <wouter.den.hollander@hotmail.com>
2022-03-31 19:04:07 +0200

    Basis for NPC

---
+++ src/com/company/simulation/Direction.java
@@ -0,0 +1,16 @@
+package com.company.simulation;
+
+public enum Direction {
+    NORTH(0, -1),
+    EAST(1, 0),
+    SOUTH(0, 1),
+    WEST(-1, 0);
+
+    private final int x;
+    private final int y;
+
+    Direction(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- src/com/company/simulation/NPC.java
+++ src/com/company/simulation/NPC.java
@@ -2,12 +2,47 @@ package com.company.simulation;
 
 import com.company.infinity.Node;
 
+import javax.imageio.ImageIO;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class NPC extends Node {
     private Point2D position;
-    private ArrayList<BufferedImage> sprites;
+    private List<BufferedImage> sprites;
+    private double speed;
+    private Direction direction;
 
+    public NPC() {
+        this.setZIndex(4);
+        setImages();
+        this.speed = 10;
+
+    }
+
+    public void update() {
+
+    }
+
+    public void draw() {
+
+    }
+
+    public void setImages() {
+        sprites = new ArrayList<>();
+        try {
+            BufferedImage image = ImageIO.read(getClass().getResourceAsStream("/assets/images/npcsprite.png"));
+            int width = image.getWidth() / 3;
+            int height = image.getHeight() / 4;
+            for (int y = 0; y < 4; y++) {
+                for (int x = 0; x < 3; x++) {
+                    this.sprites.add(image.getSubimage(x * width, y * height, width, height));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
