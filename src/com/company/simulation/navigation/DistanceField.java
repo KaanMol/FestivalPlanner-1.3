28e49a26d307a7f919caee8660f6e689760a6409
Niels Leest <n.leest1@student.avans.nl>
2022-04-03 13:47:33 +0200

    Set up a pathfinder base structure

---
+++ src/com/company/simulation/navigation/DistanceField.java
@@ -0,0 +1,12 @@
+package com.company.simulation.navigation;
+
+import com.company.simulation.map.MapArea;
+import com.company.simulation.map.TileMap;
+
+public class DistanceField {
+    private int[][] distances;
+
+    public DistanceField(MapArea area, TileMap map) {
+
+    }
+}
---
+++ src/com/company/simulation/navigation/Navigation.java
@@ -0,0 +1,19 @@
+package com.company.simulation.navigation;
+
+import com.company.simulation.map.MapArea;
+import com.company.simulation.map.TileMap;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Navigation {
+    private Map<MapArea, DistanceField> distanceFields = new HashMap<>();
+
+    public Navigation(TileMap map) {
+        for (MapArea area : map.getAreas()) {
+            distanceFields.put(area, new DistanceField(area, map));
+        }
+    }
+
+}
